3ba61bac8a0ca5420b8de23ef25f5e9cc4afe3e2
Dilippurohit47 <dilippurohit204@gmail.com>
2024-12-22 18:42:49 +0530

    checking

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World from dilip purohit");
+  res.send("Hello World from dilip ");
 });
 
 app.listen(PORT, () => {

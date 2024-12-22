103ac2e44797efe6c453ddf10a6dddb98fadae37
Dilippurohit47 <dilippurohit204@gmail.com>
2024-12-22 18:57:20 +0530

    cors added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import rateLimit from "express-rate-limit";
-
+import cors from "cors";
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World from dilip ");
+  res.send("Hello World from dilip with cors ");
 });
 
 app.listen(PORT, () => {

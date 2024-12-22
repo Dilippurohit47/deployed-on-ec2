f386cb845a7b73dd155a5348d654ca08a518b6ae
Dilippurohit47 <dilippurohit204@gmail.com>
2024-12-22 17:40:26 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import rateLimit from "express-rate-limit";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World from dilip");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
